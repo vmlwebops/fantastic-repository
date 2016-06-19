9e9fccb7ebe709494881e2f0723f7bea01eada5c
Cole R Lawrence <msgzht@gmail.com>
2016-06-19 00:10:36 -0500

    Add that comma

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -8,7 +8,7 @@ exports.config = {
   // use `npm run e2e`
   specs: [
     helpers.root('src/**/**.e2e.ts'),
-    helpers.root('src/**/*.e2e.ts')
+    helpers.root('src/**/*.e2e.ts'),
     // 'src/**/**.e2e.ts',
     // 'src/**/*.e2e.ts'
   ],
